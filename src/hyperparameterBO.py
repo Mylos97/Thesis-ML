ed9371430ca6b72579e1204de4fb1a22e03a2560
msyb <msyb@itu.dk>
2024-05-07 21:55:13 +0200

    added print to Hyperparameter BO

--- src/hyperparameterBO.py
+++ src/hyperparameterBO.py
@@ -66,7 +66,8 @@ def do_hyperparameter_BO(model_class: nn.Module,  data, in_dim:int, out_dim:int 
     baseline_parameters = ax_client.get_trial_parameters(trial_index=0)
     ax_client.complete_trial(trial_index=0, raw_data=train_evaluate(baseline_parameters))
 
-    for _ in range(TRIALS):
+    for i in range(TRIALS):
+        print(f'Started Hyperparameter BO trial {i}')
         parameters, trial_index = ax_client.get_next_trial()
         ax_client.complete_trial(trial_index=trial_index, raw_data=train_evaluate(parameters))
 
