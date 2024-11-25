14e54ee8646f246e56c08e2ecdda93cc09bcf7a5
Juri Petersen <juri@hey.com>
2024-11-25 10:58:44 +0100

    Test stepping with large and small bounds

--- src/LSBO/lsbo.py
+++ src/LSBO/lsbo.py
@@ -74,7 +74,7 @@ class LSBOResult:
         self.train_obj = torch.cat((self.train_obj, new_obj))
 
         # update progress
-        best_value = max(self.train_obj.max().item(), 0)
+        best_value = self.train_obj.max().item()
         self.best_values.append(best_value)
 
         self.state_dict = state_dict
@@ -105,8 +105,8 @@ def latent_space_BO(ML_model, device, plan, args, previous: LSBOResult = None):
     seed = 42
     latent_vector_sample = latent_vector[0].max().item()
 
-    bounds = torch.tensor([[-6] * d, [6] * d], device=device, dtype=dtype)
-    #bounds = torch.tensor([[-6_000_000] * d, [6_000_000] * d], device=device, dtype=dtype)
+    #bounds = torch.tensor([[-6] * d, [6] * d], device=device, dtype=dtype)
+    bounds = torch.tensor([[-6_000_000] * d, [6_000_000] * d], device=device, dtype=dtype)
     #bounds = torch.tensor([[-(latent_vector_sample * 25_000)] * d, [latent_vector_sample * 25_000] * d], device=device, dtype=dtype)
     #bounds = torch.stack([torch.zeros(d), torch.ones(d)])
 
@@ -153,7 +153,7 @@ def latent_space_BO(ML_model, device, plan, args, previous: LSBOResult = None):
             bounds=bounds)
         train_obj = objective_function(train_x).unsqueeze(-1)
         print(f"Train_obj: {train_obj}")
-        best_observed_value = max(train_obj.max().item(), 0)
+        best_observed_value = train_obj.max().item()
         #train_obj = torch.tensor([[0]])
 
         #initial_latency = best_observed_value
@@ -165,8 +165,10 @@ def latent_space_BO(ML_model, device, plan, args, previous: LSBOResult = None):
 
     def get_fitted_model(train_x, train_obj, state_dict=None):
         model = SingleTaskGP(
-            train_X=normalize(train_x, bounds),
+            #train_X=normalize(train_x, bounds),
+            train_X=train_x,
             train_Y=train_obj,
+            input_transform=Normalize(d=d),
             outcome_transform=Standardize(m=1)
         )
         if state_dict is not None:
@@ -242,14 +244,15 @@ def latent_space_BO(ML_model, device, plan, args, previous: LSBOResult = None):
 
         previous = LSBOResult(ML_model, model, previous.model_results, tree, previous.train_x, previous.train_obj, previous.state_dict, previous.best_values)
 
-        print(f"Best f: {max(previous.train_obj.max(), 0)}")
+        print(f"Best f: {previous.train_obj.max()}")
 
         #sampler = StochasticSampler(sample_shape=torch.Size([MC_SAMPLES]))
 
         qEI = qLogExpectedImprovement(
             model=model,
             #sampler=sampler,
-            best_f=max(previous.train_obj.max(), 0)
+            #best_f=max(previous.train_obj.max(), 0)
+            best_f=previous.train_obj.max()
         )
 
         new_x, new_obj = optimize_acqf_and_get_observation(qEI)
