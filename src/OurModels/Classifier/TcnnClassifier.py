a4f3abd93873cad43fc6ce4c47364de0bd5e8e86
mspruc <mspruc@users.noreply.github.com>
2026-03-05 10:55:25 +0100

    add simple classifier

---
+++ src/OurModels/Classifier/TcnnClassifier.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+
+from TreeConvolution.tcnn import (BinaryTreeConv, DynamicPooling,
+                                  TreeActivation, TreeLayerNorm)
+
+class TcnnClassifier(nn.Module):
+    def __init__(self, in_dim, out_dim, **args) -> None:
+        super().__init__()
+
+        self.binary_conv = nn.Sequential(
+            BinaryTreeConv(in_dim, 512),
+            TreeLayerNorm(),
+            TreeActivation(nn.Mish()),
+            BinaryTreeConv(512, 512),
+            TreeLayerNorm(),
+            TreeActivation(nn.Mish()),
+            BinaryTreeConv(512, 512),
+            TreeLayerNorm(),
+            TreeActivation(nn.Mish()),
+            BinaryTreeConv(512, 512),
+            TreeLayerNorm(),
+            TreeActivation(nn.Mish()),
+            BinaryTreeConv(512, 512),
+            TreeLayerNorm(),
+            TreeActivation(nn.Mish()),
+            BinaryTreeConv(512, 512),
+            TreeLayerNorm(),
+            TreeActivation(nn.Mish()),
+            BinaryTreeConv(512, 512),
+            TreeLayerNorm(),
+            TreeActivation(nn.Mish()),
+            BinaryTreeConv(512, 512),
+            TreeLayerNorm(),
+            TreeActivation(nn.Mish()),
+            BinaryTreeConv(512, 512),
+            TreeLayerNorm(),
+            TreeActivation(nn.Mish()),
+            BinaryTreeConv(512, 512),
+            TreeLayerNorm(),
+            TreeActivation(nn.Mish()),
+            BinaryTreeConv(512, out_dim),
+            TreeLayerNorm())
+
+    def forward(self, trees):
+        return self.binary_conv(trees)
